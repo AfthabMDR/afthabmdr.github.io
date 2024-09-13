5736847e1f75a9affceda618623a29d1f98f336e
AfthabEK <mohamedafthabek@gmail.com>
2024-09-14 01:34:57 +0530

    Added github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://afthabmdr.github.io',
+  base: 'AstroBlog',
+})
